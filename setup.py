c91c8d45e22c29b4220949769f120a648182c560
Hugo Rodger-Brown <hugo@rodger-brown.com>
2014-12-22 14:01:27 +0000

    Bump release version to 0.7.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="django-request-profiler",
-    version="0.7.1",
+    version="0.7.2",
     packages=[
         'request_profiler',
         'request_profiler.migrations',
