c146db61b4a8af4f89749f3325ef0799e4ede593
Darian Moody <mail@djm.org.uk>
2017-01-16 17:37:22 +0000

    Bump version to v0.11.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="django-request-profiler",
-    version="0.11.0",
+    version="0.11.1",
     packages=find_packages(),
     install_requires=[],
     include_package_data=True,
