5d95625416cd17c243ae363919a8cc4dcdf8daac
Darian Moody <mail@djm.org.uk>
2017-01-16 17:39:44 +0000

    Bump version for development

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="django-request-profiler",
-    version="0.11.1",
+    version="0.11.2-dev",
     packages=find_packages(),
     install_requires=[],
     include_package_data=True,
